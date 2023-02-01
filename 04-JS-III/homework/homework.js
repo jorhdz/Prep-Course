1a13fbe23c6e97f18d529472db87bf641eef1f94
Jorge Hdz <johg.0792@gmail.com>
2023-01-31 20:08:44 -0600

    Avances

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,7 +3,7 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
-  return array[0]
+return array [0]
 }
 
 
@@ -26,8 +26,12 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
-  let array=[array+1]
-  return array
+  //array.forEach(function(elemento, indice){
+  return array.map((elemento)=> elemento+1)   
+  
+   // array[indice]=elemento+1
+//});
+//return array
 }
 
 
@@ -56,7 +60,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-  return palabras.join 
+  return palabras.join(" ")
 }
 
 
@@ -76,7 +80,8 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
-  
+  let hola= numeros.reduce((acumulador, valorActual )=>acumulador + valorActual, 0 )
+  return hola 
 }
 
 
@@ -84,6 +89,8 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  let suma = resultadosTest.reduce((a,b)=>a+b,0)
+  return suma/resultadosTest.length
 }
 
 
@@ -91,7 +98,7 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-  return numeros.max
+  return Math.max(...numeros)
 }
 
 
@@ -99,16 +106,26 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length===0){
+    return 0
+    
+  }
+  if (arguments.length===1){
+    return arguments[0]
+  }
+  let producto = 1
+  for (let i=0; i<arguments.length; i++){
+    producto = producto*arguments[i] 
+  }
+  return producto
 }
+
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-  if (arreglo>18){
-    print (arreglo)
-  }
-
+  return arreglo.filter(e => e > 18).length
 }
 
 
@@ -117,8 +134,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  if (numeroDeDia){}
-  
+  if (numeroDeDia===1 || numeroDeDia===7){
+    return "Es fin de semana"
+
+
+  }
+  return "Es dia Laboral"
 } 
 
 
@@ -126,7 +147,7 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  return n.toString().startsWith("9")
 }
 
 
@@ -134,8 +155,9 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
-} 
+ 
+  return arreglo.every(e=>e===arreglo[0])
+}
 
 
 function mesesDelAño(array) {
@@ -143,6 +165,15 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  let mesesBuscados=["Enero", "Marzo" , "Noviembre"]
+  let mesesEncontrados=array.filter(mes=>mesesBuscados.includes(mes))
+  
+    
+  
+  if (mesesBuscados.length===mesesEncontrados.length){
+    return mesesEncontrados
+  }
+  return "No se encontraron los meses pedidos"
 }
 
 
