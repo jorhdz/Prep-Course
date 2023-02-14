6e06f3cad4b45a977b185592552440b927297fa7
Jorge Hdz <johg.0792@gmail.com>
2023-02-13 18:36:40 -0600

    Terminado el 07

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -25,26 +25,20 @@ const nuevoModulo = 21 % 5 === 1;
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
-function devolverString(str) {
-  return str
+
   // "Return" la string provista: str
   // Tu código:
   
-}
 
-function suma(x, y) {
-  // "x" e "y" son números
-  // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código:
-  return x+y
-  
-}
 
-function resta(x, y) {
-  // Resta "y" de "x" y devuelve el valor
-  // Tu código:
-  return x-y
-}
+const devolverString=(str)=>str
+
+
+ const suma= (x,y)=>x+y
+
+
+
+const resta = (x,y)=>{return x-y}
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
@@ -59,36 +53,32 @@ function divide(x, y) {
 }
 
 function sonIguales(x, y) {
-  // Devuelve "true" si "x" e "y" son iguales
-  // De lo contrario, devuelve "false"
+  // Devuelve "hola" si "x" e "y" son iguales
+  // De lo contrario, devuelve "adios"
   // Tu código:
-  if (x===y) {
-    return true
-    
-    
-  }
-  else {return false}
-  
+
+return x===y
+
 }
+
+
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.length===str2.length) {
-    return true
-    
-  }
-else {return false}  
+  
+
+return str1.length===str2.length
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num<90){
-  return true}
-  else {return false}
+
+
+  return num<90
 }
 
 function mayorQueCincuenta(num) {
